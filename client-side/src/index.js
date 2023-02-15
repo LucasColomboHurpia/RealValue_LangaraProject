708b97e275858c3ecca9fb72cefd60be37f8ddf6
obikakelvin <obikakelvin.2255@gmail.com>
2023-02-15 09:20:42 -0800

    fix: fixes app double render by removing strictmode

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -7,26 +7,26 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from './Components/Header';
 import MainPage from './Pages/MainPage'
 import SavedListPage from './Pages/SavedLists'
-import SearchResults from './Pages/searchResults'
+import SearchResults from './Pages/SearchResults'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  <React.StrictMode>
+  <>
 
   <Header/>
 
     <Router>
       <Routes>
-        <Route path="/searchResults" element={<SearchResults />} />
+        <Route path="/searchResults/:searchQuery" element={<SearchResults />} />
         <Route path="/savedLists" element={<SavedListPage />} />
         <Route path="/" element={<MainPage />} />
       </Routes>
     </Router>
 
-  </React.StrictMode>
+  </>
 );
 
 
