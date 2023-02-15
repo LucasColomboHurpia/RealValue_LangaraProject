f2a61ee78e8bb897323eacb4a83fd6611437e3c4
obikakelvin <obikakelvin.2255@gmail.com>
2023-02-15 09:21:49 -0800

    feat: adds property display with pin point accuracy on map

--- client-side/src/Pages/searchResults.js
+++ client-side/src/Pages/searchResults.js
@@ -1,68 +1,126 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import {
+    useParams
+} from "react-router-dom";
 
+import tt from '@tomtom-international/web-sdk-maps';
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
 import './pageStyles/searchResults.css';
 
-//RUN ON PORT 3000
-//cd client-side
-//npm start
+import ListCard from '../Components/ListCard';
 
-function SearchResults() {
+function SearchResults(props) {
+    console.log(process.env.REACT_APP_API_KEY)
+    const mapElement = useRef();
+    let { searchQuery } = useParams();
 
-  const [backendData, setBackendData] = useState([{}]);
+    const [geoCodes, setGeoCodes] = useState([]);
+    const [backendData, setBackendData] = useState([]);
+    const [mapLongitude, setMapLongitude] = useState(-121.91599);
+    const [mapLatitude, setMapLatitude] = useState(37.36765);
+    const [mapZoom, setMapZoom] = useState(10);
+    const [map, setMap] = useState({});
+    
 
-  console.log(backendData)
+    const getScrapperData = async () => {
+        try {      
+            const response = await fetch(`/scrapper?input=${searchQuery}`);
+            const data = await response.json();
+            setBackendData(data.query)
+        } catch (error) {
+            console.log(error)
+        }
+    };
 
-  //----------------------------------------
-  const [input, setInput] = useState(""); 
+    useEffect(() => {
+        getScrapperData();
+    }, []);
 
-  //form
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {      
-      const response = await fetch(`/scrapper?input=${input}`);
-      const data = await response.json();
-      let dataResults = (data[Object.keys(data)[0]])
-      console.log(dataResults)
-      setBackendData(dataResults)
-    } catch (error) {
-      console.log(error)
+    const getGeoCode = async (queries) => {
+        const batchItems = {"batchItems": queries};
+        const geoCodeURL = `https://api.tomtom.com/search/2/batch.json?key=${process.env.REACT_APP_MAP_API_KEY}`;
+        const response = await fetch(geoCodeURL, {
+            method: "POST",
+            body: JSON.stringify(batchItems),
+            headers: {
+                "content-type": "application/json;charset=utf-8"
+            }
+        });
+        const data = await response.json();
+        const geo = data.batchItems.map(item => {
+            return item.response.results[0].position
+        });
+        setGeoCodes(geo);
     }
-  };
-  //---------------------------------------------
 
-  return (
+    useEffect(() => {
+        if(backendData.length !== 0) {
+            const queries = backendData.map((data) => {
+                const fullAddress = `${data.adress1} ${data.adress2}`;
+                const query = {"query": `/geocode/${fullAddress}.json`}
+                return query;
+            })
 
-    <div>
-      <div className='inputTest'>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={input}
-            onChange={(event) => setInput(event.target.value)}
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+            getGeoCode(queries);
+        }
 
-    <div></div>
 
-      {(backendData[0].price === undefined) ?
-        (<p>Loading...</p>)
-        :
-        (backendData.map((item, i) => (
-          <>
-            <div className='postContainer'>
-              <img src={item.image} alt={item.image} className='imagePost' key={(Math.random())}></img>
-              <div key={(Math.random())}>{item.area}</div>
-              <div key={(i + Math.random())}>{item.price}</div>
-              <div key={(i + Math.random())}>{item.adress1}</div>
-              <div key={(i + Math.random())}>{item.adress2}</div>
+    }, [backendData]);
+
+    useEffect(() => {
+
+        if(geoCodes && geoCodes.length !== 0) {
+            let map = tt.map({
+                key: process.env.REACT_APP_MAP_API_KEY,
+                container: mapElement.current,
+                center: [geoCodes[0].lon, geoCodes[0].lat],
+                zoom: mapZoom
+              });
+              setMap(map);
+              return () => map.remove();
+        }
+    }, [geoCodes]);
+
+    useEffect(() => {
+
+        if(map && Object.keys(map).length !== 0 && geoCodes.length !== 0) {
+            geoCodes.forEach(geoCode => {
+                new tt.Marker().setLngLat([geoCode.lon, geoCode.lat]).addTo(map)
+                var popupOffsets = {
+                    top: [0, 0],
+                    bottom: [0, -70],
+                    "bottom-right": [0, -70],
+                    "bottom-left": [0, -70],
+                    left: [25, -35],
+                    right: [-25, -35],
+                }
+                new tt.Popup(popupOffsets);
+            })
+        }
+    }, [map, geoCodes]);
+
+
+    //----------------------------------------
+    const displayCards = () => {
+        if(backendData && backendData.length !== 0) {
+            return backendData.map((item, i) => (
+                <ListCard property={item} />
+            ))
+        }
+
+    }
+
+    //---------------------------------------------
+
+    return (
+
+        <div>
+            <div style={{ flex: '0 0 50%' }}>
+                {displayCards()}
             </div>
-          </>
-        ))
-        )}
-    </div>
-  )
+            <div style={{ height: "100vh", width: "80%" }} ref={mapElement} className="mapDiv"></div>
+        </div>
+    )
 }
 
 export default SearchResults
