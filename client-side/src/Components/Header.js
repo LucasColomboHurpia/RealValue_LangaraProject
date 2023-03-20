41ba6167fd0909d11f9015a8f479d691b8ba1e15
obikakelvin <obikakelvin.2255@gmail.com>
2023-03-19 18:42:20 -0700

    style: adds more styling to header

--- client-side/src/Components/Header.js
+++ client-side/src/Components/Header.js
@@ -7,28 +7,30 @@ import logo from '../Assets/logo.png'
 function Header() {
   const [menuOpen, setMenuOpen] = useState(true);
   return (
-    <div className="header">
-      <span className='headerContainer'>
-      <div > <Link to="/"> <img src={logo} /> </Link></div>
+    <div className='header'>
+        <div className='container'>
+            <span className='headerContainer'>
+            <div > <Link to="/"> <img src={logo} /> </Link></div>
 
-      <nav className="navbar menuDesktop">
-        <ul>
-          <li>
-            <Link to="/searchMapResults">Search Properties </Link>
-          </li>
-          <li>
-            <Link to="/savedLists">My List</Link>
-          </li>
-          <li>
-            <Link to="#">Profile</Link>
-          </li>
-        </ul>
-      </nav>
+            <nav className="navbar menuDesktop">
+                <ul>
+                <li>
+                    <Link to="/searchMapResults">Search </Link>
+                </li>
+                <li>
+                    <Link to="/savedLists">My Lists</Link>
+                </li>
+                <li>
+                    <Link to="#">Profile</Link>
+                </li>
+                </ul>
+            </nav>
 
-      <div className='navbarMobile'>
-        Mobile Menu
-      </div>
-      </span>
+            <div className='navbarMobile'>
+                Mobile Menu
+            </div>
+            </span>
+            </div>
     </div>
   );
 }
