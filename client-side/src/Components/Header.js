a4775dd9c45e5e246229934fa5b6443e300d6e6e
obikakelvin <obikakelvin.2255@gmail.com>
2023-04-01 11:12:49 -0700

    feature: adds mobile header

--- client-side/src/Components/Header.js
+++ client-side/src/Components/Header.js
@@ -1,12 +1,14 @@
 import './ComponentStyles/Header.css';
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-import logo from '../Assets/logo.png'
+import logo from '../Assets/logo.png';
+import menuIcon from '../Assets/menuIcon.svg';
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(true);
-  const [userAuthenticated, setUserAuthenticated] = useState(false);
+    const history = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(true);
+    const [userAuthenticated, setUserAuthenticated] = useState(false);
 
   useEffect(() => {
     const isLoggedIn = localStorage.getItem("isLoggedIn");
@@ -20,44 +22,55 @@ function Header() {
     if(localStorage.getItem("isLoggedIn")) setUserAuthenticated(true)
   }, [userAuthenticated])
 
+  const logout = () => {
+    localStorage.removeItem("isLoggedIn");
+    history.push('/login');
+  }
+
   return (
     <div className="header">
       <div className="container">
         <span className='headerContainer'>
-            <div className='logoHeader'>
-            <Link to="/">
-                <img src={logo} />
-            </Link>
+
+            <>
+                <img className="menuIcon" onClick={() => setMenuOpen(!menuOpen)} src={menuIcon} />
+            </>
+
+            <div className='logoHeaderContainer'>
+                <Link to="/">
+                    <img className='logoHeader' src={logo} />
+                </Link>
             </div>
 
-        <nav className="navbar menuDesktop">
-            <ul>
-            {userAuthenticated &&
-                <>
-                    <li>
-                        <Link to="/searchMapResults">Search Properties </Link>
-                    </li>
-                    <li>
-                        <Link to="/savedLists">My List</Link>
-                    </li>
-                    <li>
-                        <Link to="/profile">Profile</Link>
-                    </li>
-                </>
-            }
-                {!userAuthenticated &&
-                    <>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                    </>
-                }
-            </ul>
-        </nav>
+            <nav style={{ display: menuOpen ? 'block' : 'none' }} className="navbar menuDesktop">
+                <ul>
+                    {userAuthenticated &&
+                        <>
+                            <li>
+                                <Link to="/searchMapResults">Search</Link>
+                            </li>
+                            <li>
+                                <Link to="/savedLists">My Lists</Link>
+                            </li>
+                            <li>
+                                <Link to="/profile">Profile</Link>
+                            </li>
 
-            <div className='navbarMobile'>
-            Mobile Menu
-            </div>
+                            <li>
+                                <Link onClick={logout} to="/login">Logout</Link>
+                            </li>
+                        </>
+                    }
+                    {!userAuthenticated &&
+                        <>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                        </>
+                    }
+                </ul>
+            </nav>
+
         </span>
       </div>
     </div>
