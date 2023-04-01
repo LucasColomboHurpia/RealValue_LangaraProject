bc5648ab6c23be8b36b3f2e650813acfe7e9026d
Lucas <lucas.c.hurpia@gmail.com>
2023-04-01 11:54:48 -0700

    fix profile bug

--- client-side/src/Pages/profile.js
+++ client-side/src/Pages/profile.js
@@ -1,5 +1,6 @@
 import './pageStyles/profile.css';
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
   const [name, setName] = useState('');
